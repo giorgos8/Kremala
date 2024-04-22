1790acf16afb3c04c966fa06700b11b628011f3d
Giorgos Kokkinos <40181253+giorgos8@users.noreply.github.com>
2024-04-22 06:49:10 +0000

    get-original-code

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print('Start Kremala!!!')
+import random
+
+#lst = 'ΣΟΚΟΦΡΕΤΑ'
+#print(random.choice(lst))
+
+with open('kremala_lexeis.txt', 'r', encoding='utf8') as f:
+    word = random.choice(f.readlines()).rstrip()
+
+
+print(word)
+
+print('-'*len(word))
+
+max_wrong_tries = 3
+
+b = True
+tries = 0
+
+while b:
+
+    letter = input('Μάντεψε Γράμμα:')
+
+    if letter in word:
+        print('Μάντεψες σωστά!!!')
+    else:
+        print('Δεν βρέθηκε αυτό το γράμμα!!!d')
+        tries += 1
+
+    if tries == max_wrong_tries:
+        b = False
